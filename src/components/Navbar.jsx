9cb522ccb1d49aca659922cf70fd422f057b51c2
Ramish Bin Siddique <ramishbinsiddique24@gmail.com>
2024-04-24 18:42:09 +0500

    Adjusting Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,6 +4,10 @@ import "./Navbar.css";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const handlePopup = () => {
+    const popup = document.querySelector('.popup');
+    popup.style.top = (popup.style.top === '-120%') ? '7%' : '-120%';
+  }
   return (
     <>
       <nav>
@@ -13,12 +17,63 @@ const Navbar = () => {
         </div>
         <div className="menu">
           <ul>
-          <NavLink to="/"><li>Home</li></NavLink>
-          <NavLink to="/pvp"><li>Player vs Player</li></NavLink>
-          <NavLink to="/pvc"><li>Player vs Computer</li></NavLink>
+            <NavLink to="/" style={{ textDecoration: 'none' }}>
+              <li>Home</li>
+            </NavLink>
+            <NavLink to="/pvp" style={{ textDecoration: 'none' }}>
+              <li>Player vs Player</li>
+            </NavLink>
+            <NavLink to="/pvc" style={{ textDecoration: 'none' }}>
+              <li>Player vs Computer</li>
+            </NavLink>
+          <svg
+          onClick={handlePopup}
+            className="hamburger"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="24"
+            height="24"
+            color="#000000"
+            fill="none"
+          >
+            <path
+              d="M4 5L20 5"
+              stroke="currentColor"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M4 12L20 12"
+              stroke="currentColor"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+            <path
+              d="M4 19L20 19"
+              stroke="currentColor"
+              stroke-width="1.5"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            />
+          </svg>
           </ul>
         </div>
       </nav>
+      <div className="popup">
+      <ul>
+            <NavLink to="/" style={{ textDecoration: 'none' }}>
+              <li onClick={handlePopup}>Home</li>
+            </NavLink>
+            <NavLink to="/pvp" style={{ textDecoration: 'none' }}>
+              <li onClick={handlePopup}>Player vs Player</li>
+            </NavLink>
+            <NavLink to="/pvc" style={{ textDecoration: 'none' }}>
+              <li onClick={handlePopup}>Player vs Computer</li>
+            </NavLink>
+      </ul>
+      </div>
     </>
   );
 };
